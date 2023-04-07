fb4f865505bd0636628448c3849a1a52375bf153
codingexam01 <130130942+codingexam01@users.noreply.github.com>
2023-04-06 23:55:16 -0700

    Add files via upload

---
+++ SumOfTwo.java
@@ -0,0 +1,11 @@
+//  Instructions:
+//  Given an array of integers, print the indices of two numbers that add up to the provided target.
+//  Print only ONE solution. You may not use the same index twice.
+//  Write the time complexity of the solution in the comments
+public class SumOfTwo {
+    // Do NOT modify values below
+    final int[] INPUT_ARRAY = {1, 5, 2, 0, 4, 11, 9, 6, 12, 7};
+    final int TARGET_VAL = 9;
+
+    // Your Code here
+}
