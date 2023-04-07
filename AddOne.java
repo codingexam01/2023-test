d00eb73d33f377cd3e8a0eaa3ae66231c251a75d
codingexam01 <130130942+codingexam01@users.noreply.github.com>
2023-04-07 11:42:48 -0700

    Add files via upload

---
+++ AddOne.java
@@ -0,0 +1,19 @@
+public class AddOne {
+    // Instructions:
+    // Given an array of digits representing a number, add one to the number and output it
+    // Example:  In array [1, 2, 4, 5] this translates into number 1245.
+    // You would add one to this number and therefore the output is [1, 2, 4, 6].
+    public void Question_PlusOne() {
+
+        // Do NOT modify values below
+        final int[] INPUT_ARRAY = {4, 5, 6, 7};
+
+
+        // YOUR CODE HERE
+
+
+        //OUTPUT FINAL VALUE
+        System.out.println("\n\n-----QUESTION_ADD_ONE-----\n\n");
+
+    }
+}
