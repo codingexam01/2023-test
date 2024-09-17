fbd4c29585d17a6d6d8bceed496b1b7da632d262
codingexam01 <130130942+codingexam01@users.noreply.github.com>
2024-09-17 10:45:38 -0700

    Create valid-paranthesis.java

---
+++ valid-paranthesis.java
@@ -0,0 +1,25 @@
+/*
+* Given a String s containing just the characters '{', '}', '(', ')', '[', ']', determine if the input string is valid or not.
+* An input string is valid if:
+* 1. Open brackets must be closed by the same type of brackets.
+* 2. Open brackets must be closed in the correct order.
+* 3. Every close bracket has a corresponding open bracket of the same type
+*
+* Examples:
+* input: s = "{[]}"   output: true
+* input: s = "{][}"   output: false
+* input: s = "{(}"    output: false
+*/
+class Solution {
+  public boolean isValid(String s) {
+    // Write your code here
+  }
+
+  public static void main(String args[]) {
+    // Print your results
+    String s1 = "{[()]}";
+    String s2 = "}[](){";
+    String s3 = "{([})}";
+    
+  }
+}
